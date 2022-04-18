2df29b034e212090d307a9f8f94dbe0044a9dd9b
saraivais <isadorasaraiva95@gmail.com>
2022-04-18 16:39:16 -0300

    Feat: name input & submit button created~

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 class Login extends React.Component {
   render() {
     return (
-      <div data-testid="page-login">Componente Login</div>
+      <div data-testid="page-login">
+        <input type="text" data-testid="login-name-input" />
+        <button type="button" data-testid="login-submit-button">Entrar</button>
+      </div>
     );
   }
 }
