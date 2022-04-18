6d3bdeae9eb4e1239e9a301a11b7954fa456a084
saraivais <isadorasaraiva95@gmail.com>
2022-04-18 18:48:30 -0300

    Feat: Search Input & Button~ button disabled depending on input length~

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,46 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.enableButton = this.enableButton.bind(this);
+
+    this.state = {
+      IsButtonOff: true,
+      NameToSearch: '',
+    };
+  }
+
+  enableButton({ target }) {
+    const minimumLengthAllowed = 2;
+    const { value } = target;
+    this.setState({
+      NameToSearch: value,
+      IsButtonOff: (value.length < minimumLengthAllowed),
+    });
+  }
+
   render() {
+    const { IsButtonOff, NameToSearch } = this.state;
     return (
       <>
         <Header />
-        <div data-testid="page-search">Componente Search</div>
+        <div data-testid="page-search">
+          <input
+            type="text"
+            value={ NameToSearch }
+            data-testid="search-artist-input"
+            onChange={ this.enableButton }
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ IsButtonOff }
+          >
+            Pesquisar
+          </button>
+        </div>
       </>);
   }
 }
