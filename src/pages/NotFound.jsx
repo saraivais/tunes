9a74627a9589eebd93838994469a84b890e4311a
saraivais <isadorasaraiva95@gmail.com>
2022-04-18 16:28:36 -0300

    Feat: NotFound component~ class component created and exported~

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class NotFound extends React.Component {
+  render() {
+    return (<div data-testid="page-not-found">Componente NotFound</div>);
+  }
+}
+
+export default NotFound;
