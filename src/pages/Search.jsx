2fbbea4d7ff0d4325985a3fb55f3cd38083f565e
saraivais <isadorasaraiva95@gmail.com>
2022-04-18 18:14:24 -0300

    Feat: Header component rendered~

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Header from '../components/Header';
 
 class Search extends React.Component {
   render() {
-    return (<div data-testid="page-search">Componente Search</div>);
+    return (
+      <>
+        <Header />
+        <div data-testid="page-search">Componente Search</div>
+      </>);
   }
 }
 
