fbfe8d829434943500c1b17e34fcad1c37a41d9f
saraivais <isadorasaraiva95@gmail.com>
2022-04-18 18:14:07 -0300

    Feat: Header component rendered~

--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Header from '../components/Header';
 
 class ProfileEdit extends React.Component {
   render() {
-    return (<div data-testid="page-profile-edit">Componente ProfileEdit</div>);
+    return (
+      <>
+        <Header />
+        <div data-testid="page-profile-edit">Componente ProfileEdit</div>
+      </>);
   }
 }
 
