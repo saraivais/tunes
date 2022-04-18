fa45f98c74e045b45eff34b4eb228838c5bdc9a2
saraivais <isadorasaraiva95@gmail.com>
2022-04-18 18:13:28 -0300

    Feat: Header component rendered~

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Header from '../components/Header';
 
 class Favorites extends React.Component {
   render() {
-    return (<div data-testid="page-favorites">Componente Favorites</div>);
+    return (
+      <>
+        <Header />
+        <div data-testid="page-favorites">Componente Favorites</div>
+      </>);
   }
 }
 
