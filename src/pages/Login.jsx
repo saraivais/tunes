c85cfc6f58d5ac76da2aeab7fe80edde86a51452
saraivais <isadorasaraiva95@gmail.com>
2022-04-18 17:03:51 -0300

    Feat: enableButton function~ controls text input with NameInput state~ enables button with minimun length

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,45 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.enableButton = this.enableButton.bind(this);
+
+    this.state = {
+      IsButtonOff: true,
+      NameInput: '',
+    };
+  }
+
+  enableButton({ target }) {
+    const mininumAcceptedLength = 3;
+    const { value } = target;
+    this.setState({
+      NameInput: value,
+      IsButtonOff: (value.length < mininumAcceptedLength),
+    });
+  }
+
   render() {
+    const { IsButtonOff, NameInput } = this.state;
+
     return (
       <div data-testid="page-login">
-        <input type="text" data-testid="login-name-input" />
-        <button type="button" data-testid="login-submit-button">Entrar</button>
+        <input
+          type="text"
+          value={ NameInput }
+          data-testid="login-name-input"
+          onChange={ this.enableButton }
+        />
+        <button
+          type="button"
+          data-testid="login-submit-button"
+          disabled={ IsButtonOff }
+        >
+          Entrar
+
+        </button>
       </div>
     );
   }
