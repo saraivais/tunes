c684646b588fff48044552425c51676b4698dbb0
saraivais <isadorasaraiva95@gmail.com>
2022-04-18 16:27:39 -0300

    Feat: Profile component~ class component created and exported~

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Profile extends React.Component {
+  render() {
+    return (<div data-testid="page-profile">Componente Profile</div>);
+  }
+}
+
+export default Profile;
