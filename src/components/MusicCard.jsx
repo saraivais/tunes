1c438d93a4b7831f45d85fb9ddcd5274a08ca03a
saraivais <isadorasaraiva95@gmail.com>
2022-04-19 14:40:44 -0300

    Feat: enable fav song~ handle checkbox change using addSongs func when checked, using entire object received as prop~ conditional rendering w LoadingPage

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,20 +1,60 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import LoadingPage from './LoadingPage';
+import { addSong } from '../services/favoriteSongsAPI';
 
 class MusicCard extends React.Component {
+  constructor() {
+    super();
+
+    this.sendSongToFavorites = this.sendSongToFavorites.bind(this);
+    this.handleCheckbox = this.handleCheckbox.bind(this);
+
+    this.state = {
+      IsSendingSong: false,
+      CheckboxChecked: false,
+    };
+  }
+
+  handleCheckbox({ target }) {
+    const { entireObject } = this.props;
+    const value = target.checked;
+    this.setState({
+      CheckboxChecked: value,
+    });
+    if (value) {
+      this.sendSongToFavorites(entireObject);
+    }
+  }
+
+  async sendSongToFavorites(SongObject) {
+    this.setState({ IsSendingSong: true });
+    await addSong(SongObject);
+    this.setState({ IsSendingSong: false });
+  }
+
   render() {
-    const { MusicName, MusicURL } = this.props;
+    const { MusicName, MusicURL, MusicId } = this.props;
+    const { IsSendingSong, CheckboxChecked } = this.state;
     return (
-      <section>
-        <p>{ MusicName }</p>
-        <audio data-testid="audio-component" src={ MusicURL } controls>
-          <track kind="captions" />
-          O seu navegador não suporta o elemento
-          {' '}
-          <code>audio</code>
-          .
-        </audio>
-      </section>
+      IsSendingSong ? <LoadingPage />
+        : (
+          <section>
+            <p>{ MusicName }</p>
+            <audio data-testid="audio-component" src={ MusicURL } controls>
+              <track kind="captions" />
+              O seu navegador não suporta o elemento
+              {' '}
+              <code>audio</code>
+              .
+            </audio>
+            <input
+              type="checkbox"
+              data-testid={ `checkbox-music-${MusicId}` }
+              checked={ CheckboxChecked }
+              onChange={ this.handleCheckbox }
+            />
+          </section>)
     );
   }
 }
@@ -22,6 +62,8 @@ class MusicCard extends React.Component {
 MusicCard.propTypes = {
   MusicName: propTypes.string.isRequired,
   MusicURL: propTypes.string.isRequired,
+  MusicId: propTypes.number.isRequired,
+  entireObject: propTypes.shape().isRequired,
 };
 
 export default MusicCard;
