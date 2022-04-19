11ffcacbd14c794575601b979dd8c1ea5151ad55
saraivais <isadorasaraiva95@gmail.com>
2022-04-19 17:43:15 -0300

    ~Iniciando requisitos bonus~

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -6,6 +6,11 @@ class Favorites extends React.Component {
     return (
       <>
         <Header />
+        {/* Iniciando requisitos bônus~
+        Aqui: pegar favs da api/localstorage ->
+        usar loading page ->
+        coisar o music card com o array que a api dá ->
+        func de remover, prolly já deve dar -> loading page */}
         <div data-testid="page-favorites">Componente Favorites</div>
       </>);
   }
