9e3af4e00a033b947377655a069ed359e0ae2ca4
saraivais <isadorasaraiva95@gmail.com>
2022-04-19 21:53:08 -0300

    Feat: Profile component~ gets userinfo w getUser, conditional rendering w loadingpage, invokes UserCard component~

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,42 @@
 import React from 'react';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
+import UserCard from '../components/UserCard';
+import LoadingPage from '../components/LoadingPage';
 
 class Profile extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      UserObject: {},
+      IsLoading: false,
+    };
+  }
+
+  async componentDidMount() {
+    this.setState({ IsLoading: true });
+    const userResult = await getUser();
+    this.setState({
+      IsLoading: false,
+      UserObject: userResult,
+    });
+  }
+
   render() {
+    const { IsLoading, UserObject: { name, email, image, description } } = this.state;
     return (
       <>
         <Header />
-        <div data-testid="page-profile">Componente Profile</div>
+        {IsLoading ? <LoadingPage />
+          : (
+            <div data-testid="page-profile">
+              <UserCard
+                userName={ name }
+                userMail={ email }
+                userDesc={ description }
+                userImage={ image }
+              />
+            </div>)}
       </>);
   }
 }
